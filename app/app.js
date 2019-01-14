167783c7ad04a4c4cc7351c30629bb4776e325bd
ikliuchyk <ievgen.kliuchyk@bazaarvoice.com>
2019-01-14 11:50:53 +0200

    Add removeAll ninjas animated function

--- app/app.js
+++ app/app.js
@@ -55,8 +55,12 @@ myNinjaApp.controller('NinjaController', ['$scope', '$http', function($scope, $h
         $scope.newninja.rate = '';
     }
 
+    $scope.removeAll = function() {
+        $scope.ninjas = [];
+    };
+
     $http.get('data/ninjas.json').then(function(response) {
         $scope.ninjas = response.data;
-    })
+    });
 
 }]);
