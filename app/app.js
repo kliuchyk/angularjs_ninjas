f18efed838c4f1a5381813ca2be2e5174f5ae4ae
ikliuchyk <ievgen.kliuchyk@bazaarvoice.com>
2019-01-14 18:17:54 +0200

    Add 'contact-success' route

--- app/app.js
+++ app/app.js
@@ -8,7 +8,12 @@ myNinjaApp.config(['$routeProvider', function($routeProvider) {
             controller: 'NinjaController'
         })
         .when('/contact', {
-            templateUrl: 'views/contact.html'
+            templateUrl: 'views/contact.html',
+            controller: 'ContactController'
+        })
+        .when('/contact-success', {
+            templateUrl: 'views/contact-success.html',
+            controller: 'ContactController'
         })
         .when('/directory', {
             templateUrl: 'views/directory.html',
@@ -65,5 +70,13 @@ myNinjaApp.controller('NinjaController', ['$scope', '$http', function($scope, $h
     $http.get('data/ninjas.json').then(function(response) {
         $scope.ninjas = response.data;
     });
+
+}]);
+
+myNinjaApp.controller('ContactController', ['$scope', '$location', function($scope, $location) {
+
+    $scope.sendMessage = function() {
+        $location.path('/contact-success');
+    };
 
 }]);
